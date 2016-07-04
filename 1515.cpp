1c93d49ad224ba03ac3aca652a547aaa22164a6f
CLSW <clswrr@gmail.com>
2016-07-04 23:54:06 +0300

    Create 1515.cpp

---
+++ 1515.cpp
@@ -0,0 +1,47 @@
+# include <bits/stdc++.h>
+
+using namespace std;
+
+#define pii pair<int, int>
+#define pll pair<ll, ll>
+
+#define pb push_back
+#define eb emplace_back
+#define mp make_pair
+
+#define fi first
+#define se second
+
+typedef long long ll;
+typedef long double ld;
+typedef unsigned long long ull;
+
+const int inf = (int)1e9 + 7;
+const int maxn = (int)6e5 + 7;
+const int lmaxn = (int)8e7 + 7;
+const ll linf = (ll)1e16 + 7;
+
+const ld eps = ld(1e-11);
+
+const ll dx[] = {-1, 0, 0, 1};
+const ll dy[] = {0, -1, 1, 0};
+
+ll gcd(ll a, ll b){
+    while(b) a %= b, swap(a, b);
+    return a | b;
+}
+
+int n;
+
+int main(){
+    scanf("%d", &n);
+    int last = 0;
+    for(int i = 1; i <= n; ++i){
+        int x;
+        scanf("%d", &x);
+        if(x - last > 1) break;
+        else last += x;
+    }
+    printf("%d", last + 1);
+    return 0;
+}
