87bb8fc2601581dbf4fadba294b426f6d98216c7
CLSW <clswrr@gmail.com>
2016-07-05 00:37:02 +0300

    Create 1375.cpp

---
+++ 1375.cpp
@@ -0,0 +1,68 @@
+#include <iostream>
+#include <algorithm>
+#include <cassert>
+#include <cstdio>
+#include <climits>
+#include <iomanip>
+#include <cmath>
+#include <ctime>
+#include <map>
+#include <set>
+#include <vector>
+#include <stack>
+#include <queue>
+#include <cstring>
+#include <string>
+#include <utility>
+#include <sstream>
+#include <numeric>
+#include <functional>
+#include <bitset>
+#include <deque>
+
+using namespace std;
+
+#if __cplusplus >= 201103
+    #include <unordered_map>
+    #include <future>
+    #include <atomic>
+    #include <chrono>
+    #include <unordered_set>
+    #include <array>
+    #include <random>
+    #include <mutex>
+#endif
+
+#define pii pair<ll, ll>
+
+#define pb push_back
+#define eb emplace_back
+#define mp make_pair
+
+#define fi first
+#define se second
+
+typedef long long ll;
+typedef long double ld;
+
+const int inf = (int)1e9 + 7;
+const int maxn = (int)2e5 + 7;
+const int lmaxn = (int)1e6 + 7;
+const ll linf = (ll)1e16 + 7;
+
+const ld eps = ld(1e-11);
+
+ll k, p;
+
+main(){
+    scanf("%lld %lld", &k, &p);
+    for(ll i = 0; i < p; ++i){
+        for(ll j = 0; j < p; ++j){
+            if((i * i + j * j) % p == k){
+                printf("%lld %lld", i, j);
+                return 0;
+            }
+        }
+    }
+    return 0;
+}
