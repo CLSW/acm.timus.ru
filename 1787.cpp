c75eb0593aecf0f37cc0dd501d5df7e9b8f61933
CLSW <clswrr@gmail.com>
2016-07-05 09:05:13 +0300

    Create 1787.cpp

---
+++ 1787.cpp
@@ -0,0 +1,47 @@
+# include <bits/stdc++.h>
+
+using namespace std;
+
+#define pii pair<int, int>
+#define pll pair<ll, ll>
+
+#define pb push_back
+#define eb emplace_back
+#define mp make_pair
+
+#define fi first
+#define se second
+
+typedef long long ll;
+typedef long double ld;
+typedef unsigned long long ull;
+
+const int inf = (int)1e9 + 7;
+const int maxn = (int)1e5 + 7;
+const int lmaxn = (int)2e7 + 7;
+const ll linf = (ll)1e16 + 7;
+
+const ld eps = ld(1e-11);
+
+const ll dx[] = {-1, 0, 0, 1};
+const ll dy[] = {0, -1, 1, 0};
+
+ll gcd(ll a, ll b){
+    while(b) a %= b, swap(a, b);
+    return a | b;
+}
+
+ll n, k, all;
+
+main(){
+    cin >> n >> k;
+    for(ll i = 1; i <= k; ++i){
+        ll x;
+        cin >> x;
+        all += x;
+        all -= n;
+        all = max(0ll, all);
+    }
+    cout << all;
+    return 0;
+}
