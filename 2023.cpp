6fc7db488a7402e00e1db8e8a7e63e48f65439f2
CLSW <clswrr@gmail.com>
2016-07-05 09:11:51 +0300

    Create 2023.cpp

---
+++ 2023.cpp
@@ -0,0 +1,48 @@
+# include <bits/stdc++.h>
+
+using namespace std;
+
+#define pii pair<int, int>
+#define pll pair<ll, ll>
+
+#define pb push_back
+#define eb emplace_back
+#define mp make_pair
+
+#define fi first
+#define se second
+
+typedef long long ll;
+typedef long double ld;
+typedef unsigned long long ull;
+
+const int inf = (int)1e9 + 7;
+const int maxn = (int)1e5 + 7;
+const int lmaxn = (int)2e7 + 7;
+const ll linf = (ll)1e16 + 7;
+
+const ld eps = ld(1e-11);
+
+const ll dx[] = {-1, 0, 0, 1};
+const ll dy[] = {0, -1, 1, 0};
+
+ll gcd(ll a, ll b){
+    while(b) a %= b, swap(a, b);
+    return a | b;
+}
+
+ll n, num[1001], ans;
+string a[1001];
+map < string, ll > m;
+
+main(){
+    cin >> n;
+    m["Alice"] = m["Ariel"] = m["Aurora"] = m["Phil"] = m["Peter"] = m["Olaf"] = m["Phoebus"] = m["Ralph"] = m["Robin"] = 1;
+    m["Bambi"] = m["Belle"] = m["Bolt"] = m["Mulan"] = m["Mowgli"] = m["Mickey"] = m["Silver"] = m["Simba"] = m["Stitch"] = 2;
+    for(ll i = 1; i <= n; ++i)
+        cin >> a[i], num[i] = (!m.count(a[i]) ? 3 : m[a[i]]);
+    num[0] = 1;
+    for(ll i = 1; i <= n; ++i) ans += llabs(num[i] - num[i - 1]);
+    cout << ans;
+    return 0;
+}
